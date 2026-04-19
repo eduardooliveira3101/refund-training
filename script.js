625fbbe761f9b114baeabc31be63eff142babb2f
Eduardo Oliveira <c.edu3101@hotmail.com>
2026-04-19 14:35:58 -0300

    add expense item

---
+++ script.js
@@ -0,0 +1,102 @@
+//formulario que será enviado com as informações
+const form = document.querySelector("form");
+//O valor da despesa
+const amount = document.querySelector("#amount");
+//O nome da despesa
+const expense = document.querySelector("#expense");
+//A categoria da despesa que vamos capturar seu value e seu nome
+const category = document.querySelector("#category");
+//A lista que vai comportar os items de despesa
+const expenseList = document.querySelector("ul");
+
+//Tratamento do campo de entrada de valor
+amount.oninput = () => {
+  //Expressão regular para capturar letras
+  const regex = /\D+/g;
+
+  //Adicionando a expressão regular dentro da variável value
+  let value = amount.value.replace(regex, "");
+
+  //Adicionando a regra dentro do que o usuário estiver digitando
+  amount.value = value;
+
+  //Pegando o valor e divindo para mostrar também os centavos
+  value = Number(value) / 100;
+
+  //Adicionando a formação no que o usuário vai digitar
+  amount.value = formatCurrencyBRL(value);
+};
+
+//Ao enviar o formulário, faça tudo o que tiver dentro desse bloco
+form.onsubmit = () => {
+  //Evento padrão que vai remover o comportamento natural de form
+  event.preventDefault();
+
+  //Objeto que vai conter as informações enviadas do usuário para a construção de uma nova lista
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+
+  //Função que vai criar uma nova lista de acordo com o que o usuário mandou
+  expenseAdd(newExpense);
+};
+
+//Formatação da moeda de entrada
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  return value;
+}
+
+//Adicionando um novo item na lista de despesas
+function expenseAdd(newExpense) {
+  try {
+    //Cria o elemento para adicionar na lista
+    //Criando o elemento pai que vai abraçar o resto
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    //Criando o icon da categoria
+    const expenseIcon = document.createElement("img");
+
+    //Setando de forma dinâmica a imagem de acordo com a categoria escolhida
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+
+    //Setando o valor alternativo se a imagem nao carregar, no caso o valor seria o nome da categoria
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    //Adicionando o nome da despesa
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+
+    //Criando os detalhes dentro do expense-info
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = `${newExpense.expense}`;
+
+    //O valor em texto da categoria
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = `${newExpense.category_name}`;
+
+    //Adicionando o que foi criado dentro da estrutura
+    expenseInfo.append(expenseName, expenseCategory);
+
+    //Adicionando o icon dentro da lista criada
+    expenseItem.append(expenseIcon, expenseInfo);
+
+    //Adicionando a lista dentro de sua estrutura das listas
+    expenseList.append(expenseItem);
+
+    console.log(expenseIcon);
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas.");
+    console.log(error);
+  }
+}
